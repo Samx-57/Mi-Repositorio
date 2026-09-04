a908bb05a996efc1358bf2578394c0e2bdfcbf4b
Samx-57 <samueldavidcantillosantos@gmail.com>
2026-09-03 21:59:36 -0500

    Delete Ejercicio13.py

--- Ejercicio13.py
+++
@@ -1,6 +0,0 @@
-
-nombre = input("Digite su nombre: ")
-carrera = input("Digite su carrera: ")
-universidad = input("Digite la universidad: ")
-
-print(f"Hola {nombre}, estudias {carrera} en la {universidad}.")
