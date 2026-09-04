95d7e67fc80b9c96f65a405dc5bfef073b62e873
Samx-57 <samueldavidcantillosantos@gmail.com>
2026-09-03 21:59:13 -0500

    Delete Ejercicio2.py

--- Ejercicio2.py
+++
@@ -1,9 +0,0 @@
-
-# Ejercicio 2. Suma de dos numeros.
-
-num1 = int(input("Ingrese el primer numero: "))
-num2 = int(input("Ingrese el segundo numero: "))
-
-suma = num1 + num2
-print("===============")
-print("La suma de los dos numeros es: " + str(suma))
