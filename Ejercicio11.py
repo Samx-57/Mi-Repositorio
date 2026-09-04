84729a675d8b1a6aeafdb1f1924afac617d0770f
Samx-57 <samueldavidcantillosantos@gmail.com>
2026-09-03 21:59:26 -0500

    Delete Ejercicio11.py

--- Ejercicio11.py
+++
@@ -1,7 +0,0 @@
-
-# Parte 3. Cadenas . Ejercicio 11. Mayusculas y minusculas
-
-texto = input("Digite una palabra: ")
-
-print("En mayúsculas:", texto.upper())
-print("En minúsculas:", texto.lower())
